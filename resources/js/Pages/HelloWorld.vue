557e96e621adafb75cacf63186edf06aba07cd3c
zachtecson <zactec115@gmail.com>
2023-12-16 02:58:44 +0800

    initialize vue js

---
+++ resources/js/Pages/HelloWorld.vue
@@ -0,0 +1,6 @@
+<template>
+    <h1>Hell World</h1>
+</template>
+
+<script setup>
+</script>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('app');
 });
+
+route::get('/sample', [SampleController::class, 'index']);
